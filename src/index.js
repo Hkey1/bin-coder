fa8e30a493dfd87f0312c5d09420c5238156ce0c
Hkey1 <a.v.belousoff@gmail.com>
2025-03-11 18:58:03 +0000

    0.1.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const SafeCoder     = require('./builded/SafeCoder.js');
 const FastCoder     = require('./builded/FastCoder.js');
-const AbstractCoder = require('./builded/AbstractCoder.js');
+const AbstractCoder = require('./AbstractCoder.js');
 
 module.exports  = {SafeCoder, FastCoder, AbstractCoder, ...require('./dic/index.js')}; 
